e4091fc3bb12578dbc2a997fe057c89fa225ef91
Gvl <gulixiansimayi@gmail.com>
2023-03-22 14:40:59 +0100

    add icons

--- src/compenants/NavVerti/NavVerti.jsx
+++ src/compenants/NavVerti/NavVerti.jsx
@@ -1,10 +1,23 @@
+import {FontAwesomeIcon} from'@fortawesome/react-fontawesome'
+import {faPersonSwimming,faDumbbell,faPersonBiking} from'@fortawesome/free-solid-svg-icons'
 import './NavVerti.css'
 
 function NavVerti() {
   return (
     <div className="navVerti" >
       <div className="icons">
-
+        <div className="icon">
+          <FontAwesomeIcon icon={faPersonSwimming} />
+        </div>
+        <div className="icon">
+          <FontAwesomeIcon icon={faPersonSwimming} />
+        </div>
+        <div className="icon">
+          <FontAwesomeIcon icon={faPersonBiking} />
+        </div>
+        <div className="icon">
+          <FontAwesomeIcon icon={faDumbbell} />
+        </div>
       </div>
       <p className='navVertiText'>Copiryght, SportSee 2020</p>
     </div>
