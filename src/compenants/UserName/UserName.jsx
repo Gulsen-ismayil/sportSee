c71b21f467a3593f50fa48647cc540d3d62c3567
gul <gul@macbook-air-de-ay.home>
2023-03-27 16:50:43 +0200

    modified css

--- src/compenants/UserName/UserName.jsx
+++ src/compenants/UserName/UserName.jsx
@@ -4,8 +4,7 @@ function UserName ({user}) {
     <div>
       <h1>Bonjour {' '}
           <span style={{color:'red'}}>
-            {/* {user.data.userInfos.firstName} */}
-            ggg
+            {user.data.userInfos.firstName}
           </span>
       </h1>
       <p>Félicitation ! Vous avez explosé vos objectifs hier </p>
